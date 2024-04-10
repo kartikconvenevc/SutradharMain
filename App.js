f1fdb5742053909f5f40999c7f5650a4ab3e96ef
kartiksingalkar <kartiksingalkar6@gmail.com>
2024-04-10 22:26:31 +0530

    test change

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ const HomeScreen = ({ navigation }) => {
       <TouchableOpacity
         style={[styles.button, styles.button1]}
         onPress={() => handleAppStart('SutradharForm')}>
-        <Text style={styles.buttonText}>Form App</Text>
+        <Text style={styles.buttonText}>App1</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
